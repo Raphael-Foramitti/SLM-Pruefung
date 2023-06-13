1e0699e62fd6ac5c98b98d5ac29bbc5462bea7b4
Foramitti <foramitti.raphael67@gmail.com>
2023-06-13 10:24:57 +0200

    Anzahl der Durchläufe hinzugefügt

--- src/main/java/com/example/slmpruefung/SlmPruefungApplication.java
+++ src/main/java/com/example/slmpruefung/SlmPruefungApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SlmPruefungApplication {
 
 //@RestController leider war dies nicht möglich deshalb hart auf der Console gecodet
+
     public static void main(String[] args) {
         SpringApplication.run(SlmPruefungApplication.class, args);
-
         int zahl = 12345;
         int quersumme = berechneQuersumme(zahl);
         System.out.println("Die Quersumme von " + zahl + " ist: " + quersumme);
@@ -18,12 +18,15 @@ public class SlmPruefungApplication {
 
     public static int berechneQuersumme(int number) {
         int quersumme = 0;
+        int durchlaeufe = 0;
 
         while (number != 0) {
             quersumme += number % 10; // Addiere die letzte Ziffer zur Quersumme
             number /= 10; // Entferne die letzte Ziffer von der Zahl
+            durchlaeufe++;
         }
 
+        System.out.println("Anzahl der Durchläufe: " + durchlaeufe);
         return quersumme;
     }
 }
