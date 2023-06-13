35f171116a056fe2f18c030fed039533da3d781e
Foramitti <foramitti.raphael67@gmail.com>
2023-06-13 10:16:36 +0200

    quersumme Berechnung hinzugefügt

--- src/main/java/com/example/slmpruefung/SlmPruefungApplication.java
+++ src/main/java/com/example/slmpruefung/SlmPruefungApplication.java
@@ -6,8 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SlmPruefungApplication {
 
+//@RestController leider war dies nicht möglich deshalb hart auf der Console gecodet
     public static void main(String[] args) {
         SpringApplication.run(SlmPruefungApplication.class, args);
+
+        int zahl = 12345;
+        int quersumme = berechneQuersumme(zahl);
+        System.out.println("Die Quersumme von " + zahl + " ist: " + quersumme);
     }
 
+
+    public static int berechneQuersumme(int number) {
+        int quersumme = 0;
+
+        while (number != 0) {
+            quersumme += number % 10; // Addiere die letzte Ziffer zur Quersumme
+            number /= 10; // Entferne die letzte Ziffer von der Zahl
+        }
+
+        return quersumme;
+    }
 }
